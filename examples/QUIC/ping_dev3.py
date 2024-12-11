f57f0b731cb85276e087913c1cb75632e2942505
Laurent <laurent@xlapak.local>
2024-12-11 11:01:07 +0100

    ping

--- examples/QUIC/ping_dev3.py
+++ examples/QUIC/ping_dev3.py
@@ -23,7 +23,7 @@ def processPkt(pkt):
         if e_type == 0x86dd:
             schc_machine.schc_send(bytes(pkt)[14:])
         elif e_type == 0x0800:
-            if pkt[IP].proto == 17 and pkt[UDP].port == 0x5C4C:
+            if pkt[IP].proto == 17 and pkt[UDP].sport == 0x5C4C:
                 # got a packet in the socket
                 SCHC_pkt, device = tunnel.recvfrom(1000)
 
@@ -32,7 +32,7 @@ def processPkt(pkt):
                 origin, full_packet = schc_machine.schc_recv(
                                    schc_packet=SCHC_pkt, 
                                    device_id=other_end, 
-                                   iface='eth1',
+                                   iface='lo',
                                    verbose=True)
 
 # Start SCHC Machine
