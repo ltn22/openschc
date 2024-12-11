edd0418204f249e777f9708ae9cd73e28c93197e
Laurent <laurent@xlapak.local>
2024-12-11 14:19:20 +0100

    ping

--- examples/QUIC/gw_earth.py
+++ examples/QUIC/gw_earth.py
@@ -23,11 +23,9 @@ def processPkt(pkt):
         if e_type == 0x86dd:
             schc_machine.schc_send(bytes(pkt)[14:])
         elif e_type == 0x0800:
-            print ('.', end="")
             if pkt[IP].proto == 17 and pkt[UDP].dport == 0x5C4C:
                 # got a packet in the socket
                 SCHC_pkt, device = tunnel.recvfrom(1000)
-                print ('-', end="")
 
 
                 other_end = 'udp:'+device[0]+':'+str(device[1])
